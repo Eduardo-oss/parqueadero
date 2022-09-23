431cf92b8175fa48f79fdb75e7bec264ff81902c
Eduardo-oss <lchana@umariana.edu.co>
2022-09-23 00:03:39 -0500

    Ingresar fecha de ingreso

--- carro.py
+++ carro.py
@@ -4,3 +4,4 @@ Parqueadero Privado Carro privado
 print("ingrese la placa del carro")
 placa = input("Ingrese la placa del carro")
 modelo = input("Ingrese modelo del carro")
+fecha = input("Ingresar fecha")
